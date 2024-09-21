e9be1a27e1bae3374cea49347f178d5c59fb0a1e
SimranSalaria <157713054+SimranSalaria@users.noreply.github.com>
2024-09-21 10:18:07 +0000

    variables And dtatypes

---
+++ .devcontainer/01basics/work.js
@@ -0,0 +1,25 @@
+console.log("Hey");
+//variables -const,var,let
+const accountId=12345;
+let accountEmail="sim@gmail.com"
+var accountPass="12340"
+accountCity="Jaipur"
+console.log(accountId)
+
+// accountId=45;
+accountEmail="aerg@gamil.com"
+accountPass="5678"
+accountCity="Bengluru"
+console.table([accountEmail,accountId,accountPass,accountCity])
+let accountState;//js considered it as undefined
+console.table([accountEmail,accountId,accountPass,accountCity,accountState])
+
+//datatypes
+let age=27
+let name="simi"
+let isSleep=false
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
